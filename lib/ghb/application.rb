11a14095e297f8fa5a8214f99116ee6f6a17a53f
Yves Desgagne <yves@cloudofficer.ca>
2023-06-12 17:35:40 -0400

    Fixed default setup options

--- lib/ghb/application.rb
+++ lib/ghb/application.rb
@@ -326,7 +326,7 @@ module GHB
                   'aws-access-key-id': '${{secrets.AWS_ACCESS_KEY_ID}}',
                   'aws-secret-access-key': '${{secrets.AWS_SECRET_ACCESS_KEY}}',
                   'aws-region': '${{secrets.AWS_DEFAULT_REGION}}'
-                }
+                }.merge(setup_options)
               )
             end
 
