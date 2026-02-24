4b683b2c51db014699753c8612931ee7fd2b0b49
Yves Desgagné <yves@cloudofficer.ca>
2026-02-23 22:00:19 -0500

    Fix #291: Add 30-second timeout to HTTParty.get for gitignore API call

--- lib/ghb/gitignore_manager.rb
+++ lib/ghb/gitignore_manager.rb
@@ -38,7 +38,7 @@ module GHB
       api_url = "https://www.toptal.com/developers/gitignore/api/#{templates_param}"
 
       puts("    Detected templates: #{detected_templates.join(', ')}")
-      response = HTTParty.get(api_url)
+      response = HTTParty.get(api_url, timeout: 30)
 
       raise("Cannot fetch gitignore templates: #{response.message}") unless response.code == 200
 
